4c57edbf7e519180260e932219f45bfb9a1e6b48
Victoria Domovets <v.domovets@gmail.com>
2020-12-12 22:06:57 +0100

    блок FAQ: разметка, стилизация, адаптив, js

--- build/js/main.js
+++ build/js/main.js
@@ -2,14 +2,38 @@
 
 /* MENU */
 
-var headerBody = document.querySelector('.header__wrapper');
-var burger = document.querySelector('.burger');
+(function () {
 
-headerBody.classList.add('header__wrapper--js');
+  var headerBody = document.querySelector('.header__wrapper');
+  var burger = document.querySelector('.burger');
 
-if (burger) {
-  burger.addEventListener('click', function () {
-    headerBody.classList.toggle('header__wrapper--js');
-    burger.classList.toggle('active');
-  });
-}
+  headerBody.classList.add('header__wrapper--js');
+
+  if (burger) {
+    burger.addEventListener('click', function () {
+      headerBody.classList.toggle('header__wrapper--js');
+      burger.classList.toggle('active');
+    });
+  }
+}());
+
+
+/* FAQ */
+
+(function () {
+
+  var faqList = document.querySelector('.faq__list');
+
+  faqList.classList.remove('faq__list--nojs');
+
+  if (faqList) {
+    var toggleFaqItem = function (item) {
+      item.classList.toggle('faq__item--opened');
+    };
+
+    faqList.addEventListener('click', function (evt) {
+      var faqItem = evt.target.closest('li');
+      toggleFaqItem(faqItem);
+    });
+  }
+}());
