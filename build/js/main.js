b9e2f7d31bbd90a2c6cbe152a93fb28b2983e2a7
Victoria Domovets <v.domovets@gmail.com>
2020-12-19 00:53:48 +0100

    слайдер: разметка, стилизация, реализация

--- build/js/main.js
+++ build/js/main.js
@@ -3,7 +3,6 @@
 /* MENU */
 
 (function () {
-
   var headerBody = document.querySelector('.header__wrapper');
   var burger = document.querySelector('.burger');
 
@@ -18,10 +17,62 @@
 }());
 
 
+/* SLIDER */
+
+(function () {
+// eslint-disable-next-line no-new
+// eslint-disable-next-line no-undef
+  var swiper = new Swiper('.new__slider', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+    loop: true,
+    breakpoints: {
+      0: {
+        pagination: {
+          el: '.new__item-count',
+          clickable: true,
+          type: 'custom',
+          renderCustom: function (Swiper, current, total) {
+            return current + ' of ' + total;
+          },
+        },
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          el: '.new__item-count',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+          },
+        },
+      },
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+          el: '.new__item-count',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+          },
+        },
+      },
+    },
+    navigation: {
+      nextEl: '.new__item-next-button',
+      prevEl: '.new__item-prev-button',
+    },
+  });
+  swiper.slideNext();
+}());
+
+
 /* FAQ */
 
 (function () {
-
   var faqList = document.querySelector('.faq__list');
 
   faqList.classList.remove('faq__list--nojs');
