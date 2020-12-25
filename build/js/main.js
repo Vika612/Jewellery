f2b85528ce2d4904cd706fc697e0c720fea01b7a
Victoria Domovets <v.domovets@gmail.com>
2020-12-25 17:28:49 +0100

    pop-up login: разметка, стилизация, адаптив, js

--- build/js/main.js
+++ build/js/main.js
@@ -17,6 +17,96 @@
 }());
 
 
+/* LOGIN */
+
+(function () {
+  var ESC = 27;
+  var loginOpenBtn = document.querySelector('.user-menu__link-login');
+  var loginOpen = document.querySelector('.menu-header__link-login');
+  var loginModal = document.querySelector('.login');
+
+  if (loginModal) {
+    var loginCloseBtn = document.querySelector('.login__close');
+    var form = loginModal.querySelector('form');
+    var userEmail = form.querySelector('[name=email]');
+    var userPassword = form.querySelector('[name=password]');
+
+    var isStorageSupport = true;
+    var storage = '';
+
+    try {
+      storage = localStorage.getItem('email');
+    } catch (err) {
+      isStorageSupport = false;
+    }
+
+    if (loginOpenBtn || loginOpen) {
+
+      var openModal = function () {
+        loginModal.classList.add('login--show');
+        document.body.style.overflow = 'hidden';
+        window.addEventListener('keydown', onModalEscPress);
+        loginCloseBtn.addEventListener('click', function () {
+          closeModal();
+        });
+      };
+
+      var closeModal = function () {
+        if (loginModal.classList.contains('login--show')) {
+          loginModal.classList.remove('login--show');
+          document.body.style.overflow = '';
+        }
+        window.removeEventListener('keydown', onModalEscPress);
+      };
+
+      var onModalEscPress = function (evt) {
+        if (evt.keyCode === ESC) {
+          evt.preventDefault();
+          closeModal();
+        }
+      };
+
+      loginOpenBtn.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        openModal();
+
+        if (storage) {
+          userEmail.value = storage;
+          userPassword.focus();
+        } else {
+          userEmail.focus();
+        }
+      });
+
+      loginOpen.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        openModal();
+
+        if (storage) {
+          userEmail.value = storage;
+          userPassword.focus();
+        } else {
+          userEmail.focus();
+        }
+      });
+
+      loginModal.addEventListener('click', function (evt) {
+        var loginContent = loginModal.querySelector('.login__content');
+        if (!loginContent.contains(evt.target)) {
+          closeModal();
+        }
+      });
+
+      form.addEventListener('submit', function () {
+        if (isStorageSupport) {
+          localStorage.setItem('email', userEmail.value);
+        }
+      });
+    }
+  }
+}());
+
+
 /* FILTER */
 
 (function () {
